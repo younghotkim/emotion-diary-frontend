8ef1f527a082f6725fa1aa527832c63060067d4c
김영하 <younghotkim@gmail.com>
2024-08-22 20:57:13 +0900

    0822

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import EmotionForm from './components/EmotionForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => {
+    return (
+        <div style={{ padding: '50px', backgroundColor: '#fafafa', minHeight: '100vh' }}>
+            <EmotionForm />
+        </div>
+    );
+};
 
 export default App;
